7a40287619773ef33a64f1293b2623832e00c044
Olena Oleksandrova <ha240886kev@gmail.com>
2024-12-15 00:54:06 +0200

    fix 2

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,11 +1,12 @@
-import React, from 'react'
-import { useSelector } from 'react-redux'
-import { selectIsLoggedIn } from '../redux/auth/selectors'
+import React from 'react'
+import { useSelector } from 'react-redux';
+import { selectIsLoggedIn } from '../redux/auth/selectors';
 import { Navigate } from 'react-router-dom';
+// import PropTypes from 'prop-types';
 
-const PrivateRoute = ({children, redirectTo ='/login'}) => {
-    const isLoggedIn = useSelector(selectIsLoggedIn);
-    return isLoggedIn ? children : <Navigate to={redirectTo} />;
-}
+const PrivateRoute = ({ children, redirectTo = '/login' }) => {
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+  return isLoggedIn ? children : <Navigate to={redirectTo} />;
+};
 
 export default PrivateRoute;
